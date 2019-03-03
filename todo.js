478b85e6c5a835e9747e004e984d81e26b1a462b
qetuo2002 <qetuo2002@gmail.com>
2019-03-02 18:51:14 -0800

    basic component update

---
+++ todo.js
@@ -0,0 +1,35 @@
+const toDoform = document.querySelector(".js-toDoForm"),
+  toDoInput = form.querySelector("input");
+toDoList = document.querySelector(".js-todoList");
+
+const TODOS_LS = "toDos";
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "X";
+  const span = document.createElement("span");
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  toDoList.appendChild(li);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+}
+
+function loadToDos() {
+  const toDos = localStorage.getItem(TODOS_LS);
+  if (toDos !== null) {
+  }
+}
+
+function init() {
+  loadToDos();
+  toDoform.addEventListener("submit".handleSubmit);
+}
+
+init();
